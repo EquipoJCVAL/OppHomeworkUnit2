7eea92309ffe8129ea88b1cabb15bb8fe3215450
lescribao <lescribaoliva@gmail.com>
2022-08-25 20:32:05 +0200

    Added queries from SQL and updated gitignore

--- src/main/java/com/Unit2/OppHomeworkUnit2/repository/SalesRepRepository.java
+++ src/main/java/com/Unit2/OppHomeworkUnit2/repository/SalesRepRepository.java
@@ -1,7 +1,27 @@
 package com.Unit2.OppHomeworkUnit2.repository;
 
+import com.Unit2.OppHomeworkUnit2.model.Enums.Status;
+import com.Unit2.OppHomeworkUnit2.model.Lead;
+import com.Unit2.OppHomeworkUnit2.model.Opportunity;
+import com.Unit2.OppHomeworkUnit2.model.SalesRep;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface SalesRepRepository {
+
+    public List<Lead> findLeadBySalesRep(SalesRep salesRep);
+
+    public List<Opportunity> findOpportunityBySalesRep(SalesRep salesRep);
+
+    @Query("SELECT * FROM Opportunity WHERE status LIKE 'CLOSED_WON' AND WHERE SalesRep LIKE :salesRep")
+    public List<Opportunity> findOpportunityBySalesRepAndStatusWon(SalesRep salesRep);
+
+    @Query("SELECT * FROM Opportunity WHERE status LIKE 'CLOSED_LOST' AND WHERE SalesRep LIKE :salesRep")
+    public List<Opportunity> findOpportunityBySalesRepAndStatusLost(SalesRep salesRep);
+
+    @Query("SELECT * FROM Opportunity WHERE status LIKE 'OPEN' AND WHERE SalesRep LIKE :salesRep")
+    public List<Opportunity> findOpportunityBySalesRepAndStatusOpen(SalesRep salesRep);
 }
